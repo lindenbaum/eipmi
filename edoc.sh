78e1337739256ee71030417846a1fbe8b2f53e03
Tobias Schlager <tobias.schlager@lindenbaum.eu>
2012-10-30 11:43:36 +0100

    Add script for automatic site generation/deployment

--- edoc.sh
+++ edoc.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
 # Script to generate EDoc documentation and optionally deploy it
-# to the github gh-pages branch
+# to the github gh-pages branch. The current branch must be clean
+# when executing this script.
 # Usage:
 #   ./edoc.sh        - will only generate the documentation
 #   ./edoc.sh deploy - will do the above and deploy to github
 
-# get edown from github
 mkdir -p tmp
+mkdir -p doc
+
+# get edown from github
 git clone https://github.com/hdiedrich/markedoc.git tmp
 
-# create overview.edoc
-mkdir -p doc
-perl -pi -e "s/\`\`\`/\'\'\'/g" README.md
+# preprocess README.md because markdown.sed can't handle code blocks
+perl -pi.bak -e "s/\`\`\`/\'\'\'/g" README.md
 perl -pi -e "s/\'\'\'erlang/\`\`\`/g" README.md
+
+# create overview.edoc
 echo "@title EIPMI - A native Erlang IMPI library" > doc/overview.edoc
 echo "@copyright 2012 Lindenbaum GmbH" >> doc/overview.edoc
 echo "" >> doc/overview.edoc
 sed -r -f tmp/bin/markedoc.sed README.md >> doc/overview.edoc
-git checkout README.md
+
+# restore original README.md
+mv README.md.bak README.md
 
 # create edoc and cleanup
 rebar clean compile doc
@@ -26,6 +32,7 @@ rm -rf tmp
 
 # deploy to gh-pages
 if [ "$1" == "deploy" ]; then
+    CURRENT=`git branch | grep "*" | awk '{print $2}'`
     git checkout gh-pages
     rm *.html
     rm edoc-info
@@ -35,5 +42,5 @@ if [ "$1" == "deploy" ]; then
     git add .
     git commit -a -m "Regenerated site documentation."
     git push origin gh-pages
-    git checkout master
+    git checkout $CURRENT
 fi
